5f978a09615ec1cb6aa23fe9999f5ee5d8138054
agnieszkahoza <agnieszka.hoza@gmail.com>
2025-06-02 18:51:55 +0200

    add(image-data): ImageData with new set of images and favorites

--- src/data/ImageData.js
+++ src/data/ImageData.js
@@ -1,32 +1,174 @@
-const initialImages = [
-{
-"id": 1,
-"title": "Image 1",
-"upvotes": 2,
-"downvotes": 1,
-"image": "https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
-},
-{
-"id": 2,
-"title": "Image 2",
-"upvotes": 0,
-"downvotes": 3,
-"image": "https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-},
-{
-"id": 3,
-"title": "Image 3",
-"upvotes": 3,
-"downvotes": 0,
-"image": "https://images.pexels.com/photos/17483869/pexels-photo-17483869/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-represents-how-machine-learning-is-inspired-by-neuroscience-and-the-human-brain-it-was-created-by-novoto-studio-as-par.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-},
-{
-"id": 4,
-"title": "Image 4",
-"upvotes": 0,
-"downvotes": 0,
-"image": "https://images.pexels.com/photos/18069239/pexels-photo-18069239/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-represents-how-ai-powered-tools-can-support-us-and-save-time-it-was-created-by-martina-stiftinger-as-part-of-the-visua.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-}
-]
+import epoch from "./01epoch.png";
+import rekindled from "./02rekindled.png";
+import solstice from "./03solstice.png";
+import equinox from "./04equinox.png";
+import witchesdragons from "./05witchesdragons.png";
+import horizon from "./horizon01.png";
+import mirapor from "./Mirapor01.png";
+import mithra1 from "./mithra01.png";
+import mithra2 from "./mithra02.png";
+import motra from "./motra01.png";
+import phatasma from "./phatasma01.png";
+import silentRiver from "./silentRiver01.png";
+import silverRiver from "./silverRiver02.png";
+import spark from "./spark01.png";
+import tunhil from "./tunhil01.png";
+import village from "./village01.png";
+import voidImage from "./void01.png";
+import wellOfLife from "./wellOfLife01.png";
+import yergamond from "./Yergamond01.png";
 
-export default initialImages;
+export const initialImages = [
+  {
+    id: 1,
+    favorite: false,
+    title: "Epoch of Fire",
+    upvotes: 15,
+    downvotes: 2,
+    image: epoch,
+  },
+  {
+    id: 2,
+    favorite: false,
+    title: "Rekindled Flames",
+    upvotes: 12,
+    downvotes: 3,
+    image: rekindled,
+  },
+  {
+    id: 3,
+    favorite: false,
+    title: "Summer Solstice",
+    upvotes: 8,
+    downvotes: 4,
+    image: solstice,
+  },
+  {
+    id: 4,
+    favorite: false,
+    title: "Autumn Equinox",
+    upvotes: 7,
+    downvotes: 5,
+    image: equinox,
+  },
+  {
+    id: 5,
+    favorite: false,
+    title: "Witches and Dragons",
+    upvotes: 18,
+    downvotes: 1,
+    image: witchesdragons,
+  },
+  {
+    id: 6,
+    favorite: false,
+    title: "Distant Horizon",
+    upvotes: 6,
+    downvotes: 6,
+    image: horizon,
+  },
+  {
+    id: 7,
+    favorite: false,
+    title: "The City of Mirapor",
+    upvotes: 9,
+    downvotes: 3,
+    image: mirapor,
+  },
+  {
+    id: 8,
+    favorite: false,
+    title: "Mithra's First Vision",
+    upvotes: 13,
+    downvotes: 2,
+    image: mithra1,
+  },
+  {
+    id: 9,
+    favorite: false,
+    title: "Mithra's Second Vision",
+    upvotes: 11,
+    downvotes: 4,
+    image: mithra2,
+  },
+  {
+    id: 10,
+    favorite: false,
+    title: "Motra",
+    upvotes: 5,
+    downvotes: 7,
+    image: motra,
+  },
+  {
+    id: 11,
+    favorite: false,
+    title: "Queen of Scoundrels",
+    upvotes: 3,
+    downvotes: 9,
+    image: phatasma,
+  },
+  {
+    id: 12,
+    favorite: false,
+    title: "Silent River 1",
+    upvotes: 4,
+    downvotes: 8,
+    image: silentRiver,
+  },
+  {
+    id: 13,
+    favorite: false,
+    title: "Silver River 2",
+    upvotes: 7,
+    downvotes: 5,
+    image: silverRiver,
+  },
+  {
+    id: 14,
+    favorite: false,
+    title: "Spark",
+    upvotes: 14,
+    downvotes: 2,
+    image: spark,
+  },
+  {
+    id: 15,
+    favorite: false,
+    title: "Tunhil",
+    upvotes: 6,
+    downvotes: 6,
+    image: tunhil,
+  },
+  {
+    id: 16,
+    favorite: false,
+    title: "Village",
+    upvotes: 10,
+    downvotes: 3,
+    image: village,
+  },
+  {
+    id: 17,
+    favorite: false,
+    title: "The Void",
+    upvotes: 2,
+    downvotes: 10,
+    image: voidImage,
+  },
+  {
+    id: 18,
+    favorite: false,
+    title: "Well of Life",
+    upvotes: 16,
+    downvotes: 1,
+    image: wellOfLife,
+  },
+  {
+    id: 19,
+    favorite: false,
+    title: "Yergamond Castle",
+    upvotes: 8,
+    downvotes: 4,
+    image: yergamond,
+  },
+];
